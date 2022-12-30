f5291bf62cd15068ce0a56eaf76514d14dd04e4e
DJ - 11TM 702160 - <120533218+702160@users.noreply.github.com>
2022-12-30 18:26:40 +0000

    Tried a different method of doing the score tracker as a database instead of as something that pops up on screen.

--- main.py
+++ main.py
@@ -11,34 +11,79 @@ import random
 import turtle
 global score 
 global high_score 
+from tabulate import tabulate
 
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 220)
-pen.write("Win Streak: 0", align="center", font=("times new roman", 20, "normal"))
+#Establishes what the table will look like
+file = 'test.txt'
+database = []
+record = ['','']
+question = ''
+DELIMITER = "|"
 
-score = 0
+"""
+Function reads a flat file, bar separated into a multi dimensional array
+inputs:file Name
+outputs:list
+"""
+def readFileIntoList(read_file):
+  global file
+  try:
+    with open(file, 'r') as reader:
+        database = [line.strip().split(DELIMITER) for line in reader]
+        
+        #temporary print of contents of list
+        for row in range(len(database)):
+          for column in range(len(database[row])):
+            print(database[row][column],end=DELIMITER)
+          print()
 
-#Intro to the hangman game and instuctions
-print(" ")
-remember = TextDelay.delay_print("Welcome to Hangman")
-print(" ")
-time.sleep(2)
+    return database      
+  except IOError:
+    print("File not accessible")
+  except FileNotFoundError:
+    print("File does not exist")
+
+"""
+Function removes any empty lines from the given file
+inputs:none
+outputs:none
+"""
+def remove_empty_lines():
+  try:
+    global file
+    with open(file, 'r+') as f:
+        lines = f.readlines()
+        f.seek(0)
+        f.writelines(line for line in lines if line.strip())
+        f.truncate()
+  except IOError:
+     print(" \033[1;31m File not accessible")
+  except FileNotFoundError:
+     print(" \033[1;31m File does not exist")
+
+"""
+Function writes the contents of the database back out to the file for storage
+inputs:database array
+outputs:none
+"""
+def write_database(database):
+  try:
+     global file
+     with open(file, 'w') as writer:
+        for element in database:
+          writer.write(DELIMITER.join((str(x) for x in element)) + '\n')
   
-def clear_console_long():
-  sleep (3)
-  os.system('clear')
-  
-def clear_console_instant():
-  sleep (0.2)
-  os.system('clear')
+     remove_empty_lines()
+  except IOError:
+     print(" \033[1;31m File not accessible")
+  except FileNotFoundError:
+     print(" \033[1;31m File does not exist")      
 
-# The parameters we require to execute the game:
-  
+"""
+Function prompts the user to respond to a question that contains only valid answers
+inputs:question, list of valid answers
+outputs:valid answer
+"""
 def ask(question, validList):
   while True:
     answer = input(question)
@@ -48,6 +93,44 @@ def ask(question, validList):
       print(" \033[1;31m Sorry, invalid answer, please select from the following")
       print(validList)  
   return answer
+
+"""
+Function displays a 3 column table nicely formatted using the tabulate package
+inputs:2d list with headers first, last, no
+outputs:none (statements to console)
+"""
+def display(database,header):
+
+  table = tabulate(database, headers=header, tablefmt='orgtbl')
+  print(table)
+
+"""
+Function adds a record, ensures no duplicate student number
+inputs:array database
+outputs:array database
+"""
+def add(database):
+    record = ['','',]
+    record[0] = input(" \033[1;34m Enter Name:")
+    found = False
+    for row in range(len(database)):
+      if database[row][0] == record[0]:
+        found = True
+    if found:  
+      print(" \033[1;31m Sorry, no duplicates please")  
+    else:  
+      database.append(record)  
+
+#Get the previous version of the database last accessed
+database = readFileIntoList(file)
+  
+def clear_console_long():
+  sleep (3)
+  os.system('clear')
+  
+def clear_console_instant():
+  sleep (0.2)
+  os.system('clear')
   
 def main():
     global count
@@ -174,20 +257,26 @@ def hangman():
           
     if word == '_' * length:
         print("Congrats! You have guessed the word correctly!")
-        score += 1
         clear_console_long()
         turtle.clear()
         
     elif count != limit:
         hangman()
-  
+
 while True:
-  question = ask("\033[1;35m Would you like to play hangman?(y/n)? ", ['y','yes','Yes','n','no','No'])
-  if question in ['y','yes','Yes']:
-    pen.write("Score: {}".format(score), align="center", font=("times new roman", 20, "normal"))
-    main ()
-    hangman()
-  elif question in ['n','no','No']:
-    print("Thanks for playing!")
-    break
-    
+  print("\033[1;34m Welcome to Hangman")
+  question = ask("\033[1;34m What would you like to do (play, display leaderboard, exit)? ", ['play','display leaderboard','exit'])
+
+  if question == "play":
+    database = add(database)
+    main()
+    hangman()  
+  elif question == "display leaderboard":
+    display(database,['Name', 'Total Score'])
+  elif question == "exit":
+    TextDelay.delay_print_fast(" \033[1;32m Thanks for using!")
+    break 
+  
+#write the contents of the array back to the file for next time
+write_database(database)
+  
